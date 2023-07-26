1f7f0e62c73600ecec7162f0e7515bae19523d3d
pablo2637 <pabli2637@gmail.com>
2023-07-26 13:58:47 +0200

    admin

--- controllers/controllerVisit.js
+++ controllers/controllerVisit.js
@@ -10,7 +10,98 @@ const { sendError } = require('./controllerMailer');
 
 const checkDates = (a, b) => {
     return new Date(b.date) - new Date(a.date);
-}
+};
+
+
+
+const processData = dataVisits => {
+
+    const newVisits = [];
+
+    dataVisits.forEach(vis => {
+
+        const date = new Date(vis.date).toLocaleString('es-ES', { dateStyle: 'long' });
+        // console.log('date', date);
+
+        const oldInd = newVisits.findIndex(el => el.date === date);
+        let newInd = -1, ipInd = -1;
+
+        if (oldInd != -1) {
+
+            ipInd = newVisits[oldInd].ips.findIndex(el => el.ip === vis.ip);
+            // console.log('ipInd', ipInd, vis.date);
+
+            if (ipInd != -1)
+                newVisits[oldInd].ips[ipInd].logs.push(vis.city ?
+                    {
+                        event: 'Visit',
+                        date: vis.date
+                    } :
+                    {
+                        event: vis.event,
+                        devMode: vis.devMode,
+                        lang: vis.lang,
+                        date: vis.date
+                    });
+
+            else
+                newVisits[oldInd].ips.push({
+                    ip: vis.ip, logs: [vis.city ?
+                        {
+                            event: 'Visit',
+                            date: vis.date
+                        } :
+                        {
+                            event: vis.event,
+                            devMode: vis.devMode,
+                            lang: vis.lang,
+                            date: vis.date
+                        }]
+                });
+
+        } else {
+
+            newVisits.push({ date, ips: [] });
+            newInd = newVisits.length - 1;
+            newVisits[newInd].ips.push({ ip: vis.ip });
+
+            newVisits[newInd].ips[newVisits[newInd].ips.length - 1].logs = [(vis.city ?
+                {
+                    event: 'Visit',
+                    date: vis.date
+                } :
+                {
+                    event: vis.event,
+                    devMode: vis.devMode,
+                    lang: vis.lang,
+                    date: vis.date
+                })
+            ];
+
+        }
+
+        const index = oldInd === -1 ? newInd : oldInd;
+        const ipIndex = ipInd === -1 ? newVisits[index].ips.length - 1 : ipInd;
+        // console.log('index', index, 'ipIndex', ipIndex);
+
+        if (vis.org && !newVisits[index].ips[ipIndex].city) {
+
+            const { city, region, country_name, ip, org, postal, latitude, longitude } = vis;
+
+            newVisits[index].ips[ipIndex] = {
+                ...newVisits[index].ips[ipIndex],
+                city, region, country_name, ip, org, postal, latitude, longitude
+            };
+
+            // console.log('newV', newVisits);
+        }
+
+
+    });
+
+    // console.log('nV', newVisits);
+    return newVisits;
+};
 
 /**
 * Devuelve todos las visitas.
@@ -34,12 +125,15 @@ const getVisits = async (req, res) => {
                 data: []
             });
 
+        const newVisits = processData(visits.sort(checkDates));
+
         return res.status(200).json({
             ok: true,
             msg: 'Visitas encontradas con éxito',
             length: visits.length,
-            last: visits[visits.length - 1],
-            data: visits.sort(checkDates).slice(0, 5)
+            last: visits[0],
+            data: newVisits
+            // data: visits.sort(checkDates).slice(0, 5)
         });
 
     } catch (e) {
